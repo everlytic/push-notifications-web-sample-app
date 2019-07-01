ba050262fe9daadab2eeafa86940ee2b0fdf71ac
Luke <luke@everlytic.com>
2019-07-01 10:58:13 +0200

    Changed the submit to a form to validate the email input

--- example-app.js
+++ example-app.js
@@ -3,34 +3,37 @@ window.addEventListener('load', function() {
     SDK.init({
         hash:"cD1lMjliNjNmNC05M2JlLTQxY2ItYmQ3NC0wM2Q0MDUyNzMzOWU7aT1odHRwOi8vbG9jYWwuZXZlcmx5dGljLmNvbTtwdWJrPUJDdnFhcm9pZHAzRnJvOGd6TGxFeXF5aHFmc2JqZVhXWkI2Mm9LQlRFZDBlNXVNWkNrWFllTGVqNXBKeWtkQU81ck9ZM1Y4Zk1tWXNNQzVFLXg0NEtjTQ==",
         // autoSubscribe: true, // Commented out so that we have full control over the user's subscribe
-        debug: true,
+        // debug: true, // Put this in while are developing to get detailed error messages and disable pre-flight caching.
     });
 
-    let subscriptionButton = document.querySelector('#push-subscription-button');
-    subscriptionButton.addEventListener('click', function() {
+    document.querySelector('#push-subscription-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+
         changePushButtonState('computing');
         if (!window.localStorage.getItem('isPushEnabled')) {
             const email = document.querySelector('#push-subscription-email').value;
 
             if (isEmailValid(email)) {
-                SDK.subscribe({"email" : email}).then(function(){
+                SDK.subscribe({"email" : email}).then(function() {
                     window.localStorage.setItem('isPushEnabled', 'yes');
                     changePushButtonState('enabled');
-                }).catch(function(){
+                }).catch(function(err) {
+                    console.error(err);
                     window.localStorage.removeItem('isPushEnabled');
                     changePushButtonState('disabled');
                 });
             } else {
-                SDK.subscribeWithAskEmailPrompt().then(function(){
+                SDK.subscribeWithAskEmailPrompt().then(function() {
                     window.localStorage.setItem('isPushEnabled', 'yes');
                     changePushButtonState('enabled');
-                }).catch(function(){
+                }).catch(function(err) {
+                    console.error(err);
                     window.localStorage.removeItem('isPushEnabled');
                     changePushButtonState('disabled');
                 });
             }
         } else {
-            SDK.unsubscribe().then(function(){
+            SDK.unsubscribe().then(function() {
                 window.localStorage.removeItem('isPushEnabled');
                 changePushButtonState('disabled');
             });
@@ -52,24 +55,26 @@ window.addEventListener('load', function() {
     }
 
     function changePushButtonState(state) {
+        let subscriptionButton = document.querySelector('#push-subscription-button');
+
         switch (state) {
             case 'enabled':
                 subscriptionButton.disabled = false;
-                subscriptionButton.textContent = 'Disable Push notifications';
+                subscriptionButton.value = 'Disable Push notifications';
                 subscriptionButton.className = 'btn btn-outline-danger';
                 break;
             case 'disabled':
                 subscriptionButton.disabled = false;
-                subscriptionButton.textContent = 'Enable Push notifications';
+                subscriptionButton.value = 'Enable Push notifications';
                 subscriptionButton.className = 'btn btn-outline-success';
                 break;
             case 'computing':
                 subscriptionButton.disabled = true;
-                subscriptionButton.textContent = 'Loading...';
+                subscriptionButton.value = 'Loading...';
                 break;
             case 'incompatible':
                 subscriptionButton.disabled = true;
-                subscriptionButton.textContent = 'Push notifications are not compatible with this browser';
+                subscriptionButton.value = 'Push notifications are not compatible with this browser';
                 subscriptionButton.className = 'btn btn-danger';
                 break;
             default:
